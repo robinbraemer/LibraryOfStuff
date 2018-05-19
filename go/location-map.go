5a0638408143cfa65f7078bd0fa564b169ec8f51
RoboFlax | Robin <roboflax@gmail.com>
2018-05-19 17:14:16 +0200

    toString for type and stuff...

--- go/location-map.go
+++ go/location-map.go
@@ -7,6 +7,11 @@ type Vertex struct {
 	Lat, Long float64
 }
 
+type Location struct {
+	Name string
+	Loc  Vertex
+}
+
 var m = map[string]Vertex{
 	"Bell Labs": Vertex{
 		40.68433, -74.39967,
@@ -18,11 +23,29 @@ var m = map[string]Vertex{
 
 func main() {
 	fmt.Println(toString(m))
+
+	var locs []Location
+	for k, v := range m {
+		locs = append(locs, Location{k, v})
+	}
+
+	for _, loc := range locs {
+		fmt.Println(loc.toString())
+	}
+}
+
+func (l Location) toString() string {
+	var s string
+	s += "Name: " + l.Name + "\n"
+	s += "Location:\n"
+	s += "  Lat: " + FloatToString(l.Loc.Lat) + "\n"
+	s += "  Long: " + FloatToString(l.Loc.Long) + "\n"
+	return s
 }
 
 func FloatToString(input_num float64) string {
-    // to convert a float number to a string
-    return strconv.FormatFloat(input_num, 'f', 6, 64)
+	// to convert a float number to a string
+	return strconv.FormatFloat(input_num, 'f', 6, 64)
 }
 
 func toString(m map[string]Vertex) string {
